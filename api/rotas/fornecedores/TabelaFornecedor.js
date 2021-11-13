35fafe976c2033cdbda9e5cd73c3e1b0685d70e6
Cintia Lima <cintia.rl94@gmail.com>
2021-11-13 10:51:08 -0300

    PConteúdo das aulas 1 e 2

---
+++ api/rotas/fornecedores/TabelaFornecedor.js
@@ -0,0 +1,21 @@
+const Modelo = require('./ModeloTabelaFornecedor')
+
+module.exports = {
+    listar() {
+        return Modelo.findAll();
+    },
+    inserir(fornecedor) {
+        return Modelo.create(fornecedor)
+    },
+    async buscarPorId(id) {
+        const fornecedorEncontrado = await Modelo.findOne({
+            where: {
+                id: id
+            }
+        })
+        if (!fornecedorEncontrado) {
+            throw new Error('Fornecedor não encontrado')
+        }
+        return fornecedorEncontrado
+    }
+}
